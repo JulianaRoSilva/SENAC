09fd141dfd3f6add5b3d8a2f61b2a9e86be8f4c2
Juliana Silva <rsilvajuju@gmail.com>
2026-07-16 19:56:54 -0300

    Add files via upload

--- UC3/Scripts_FINAL.sql
+++ UC3/Scripts_FINAL.sql
@@ -1,3 +1,5 @@
+-- CRIAÇÃO DE TABELAS
+
 -- =========================================================
 -- TABELA: Equipes
 -- =========================================================
@@ -80,7 +82,7 @@ CREATE TABLE participacao (
 	REFERENCES jogador(id) 
 );
 
------------------------------------------------------------
+-- INSERÇÃO DE VALORES NAS TABELAS
 
 INSERT INTO equipes(nome) VALUES
 ('SQL power'),('emanuGo'),('bombinhas'),
@@ -96,7 +98,6 @@ INSERT INTO jogador(nome, id_equipes) VALUES
 ('Willian', 5),('Tomas', 5),('Bruno', 5),('Gabriel', 5),
 ('Silvio', 6),('Bruno', 6),('Dionatan', 6),('Vinicius', 6);
 
-update jogador set id_equipes = 3 where id = 25
 
 /*EQUIPES:
 SQLPOWER: RAFA, FOFONKA, WELLINGTON, ERICK: 5   
@@ -114,7 +115,6 @@ INSERT INTO jogo(nome,genero) VALUES
 ('Street Fighter','Luta'),
 ('Bomber Man','Arcade');
 
-
 -- =========================================================
 
 INSERT INTO campeonato(nome, data_inicio)
@@ -122,7 +122,8 @@ VALUES ('Campeonato Retrô 2026','2026-07-10');
 
 
 -- =========================================================
-/*Partidas que vai contar o id do jogo, e o id do campeonato. 
+/*
+Partidas que vai contar o id do jogo, e o id do campeonato. 
 Será o controle de qual jogo vai ser jogado pelos jogadores
 */ 
 
@@ -132,14 +133,14 @@ INSERT INTO partida(id_jogo, id_campeonato) VALUES
 -- =========================================================
 
 -- consulta auxiliar
-select jogador.id as idJogador, jogador.nome as jogador, equipes.id as idEquipe, equipes.nome as nomeEquipe from jogador
-inner join equipes on jogador.id_equipes = equipes.id order by id_equipes
+select jogador.id as idJogador, jogador.nome as jogador, equipes.id as idEquipe, equipes.nome 
+as nomeEquipe from jogador
+inner join equipes on jogador.id_equipes = equipes.id 
+order by jogador.nome
 
 -- =========================================================
 -- 1 primeira rodada: mario kart id 1
 -- =========================================================
-
-select * from participacao order by id_jogador
 
 INSERT INTO participacao(id_partida,id_jogador,pontuacao) VALUES
 -- GA
@@ -162,22 +163,104 @@ INSERT INTO participacao(id_partida,id_jogador,pontuacao) VALUES
 
 
 -- =========================================================
--- 2 segunda rodada: Street Fighter
+-- 2 segunda rodada: Street Fighter 2
+-- =========================================================
+select * from participacao
+
+INSERT INTO participacao(id_partida,id_jogador,pontuacao) VALUES
+-- GA
+(2,23,0),(2,9,5),
+(2,22,0),(2,7,5),
+(2,7,5),(2,8,5),
+
+-- GB
+(2,15,0),(2,2,5),
+(2,22,0),(2,2,5),
+(2,12,0),(2,17,5),
+
+-- SEMI FINAL
+(2,2,5),(2,18,0),
+(2,6,5),(2,8,0),
+
+-- FINAL
+(2,2,50),(2,5,25);  
+-- =========================================================
+-- 3 terceira rodada: Bomber Man 3
 -- =========================================================
 
+INSERT INTO participacao(id_partida,id_jogador,pontuacao) VALUES
+-- GA
+(3,6,5),(3,21,0),
+(3,6,5),(3,18,0),
+(3,17,5),(3,22,0),
 
+-- GB
+(3,3,0),(3,8,5),
+(3,8,5),(3,15,0),
+(3,15,5),(3,2,0),
+
+-- SEMI FINAL
+(3,6,0),(3,15,5),
+(3,18,5),(3,8,0),
+
+-- FINAL
+(3,15,25),(3,18,50); -- campea alison
 
 -- =========================================================
--- 3 terceira rodada: Bomber Man 
+-- 2 segunda rodada: Mortal Kombat 4
 -- =========================================================
 
-(codex_tro,5), (só sesenha,0)
-(codex_tro,5), (selectplayers,0)
-(só resenha,5),(selectplayers,0)
+INSERT INTO participacao(id_partida,id_jogador,pontuacao) VALUES
 
--- =========================================================
--- final
--- =========================================================
+-- GA
+(4,17,15),(4,11,0),
+(4,1,10),(4,17,5),
+(4,17,10),(4,15,5),
 
-(codex_tro,5), (só resenha,0)
-(codex_tro,25), (só resenha,50)
+-- GB
+(4,9,10),(4,6,5),
+(4,6,10),(4,24,5),
+(4,25,10),(4,9,5),
+
+-- SEMI FINAL
+(4,3,20),(4,24,0),
+(4,17,10),(4,6,20),
+
+-- FINAL
+(4,6,50),(4,24,25); -- campea ju
+
+-- ===================================
+
+-- CONSULTAS
+
+/*
+1. Quais jogadores pertencem a cada equipe?
+2. Quais partidas foram disputadas?
+Exiba:
+
+jogo;
+data;
+rodada;
+participantes;
+pontuação obtida.
+
+3. Qual foi a pontuação total de cada equipe?
+4. Qual é o ranking das equipes?
+5. Qual é o ranking dos jogadores?
+6. Qual foi a maior pontuação registrada em cada jogo?
+7. Qual é a média de pontos obtida por cada equipe?
+8. Qual foi a menor e a maior pontuação obtida por cada equipe?
+9. Quantas partidas foram disputadas em cada jogo?
+10. Quantas partidas cada jogador disputou?
+11. Quais jogadores participaram de mais de uma partida?
+Utilize a cláusula HAVING.
+12. Gere uma classificação final do campeonato contendo:
+
+jogador;
+equipe;
+partidas disputadas;
+pontuação total.
+Ordene os resultados da maior para a menor pontuação.
+*/
+
+
